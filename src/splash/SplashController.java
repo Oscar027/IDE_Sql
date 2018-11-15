31dc21b615aeb68d18e20e42bd92263d2a9d1b72
jhonatanmartir <jhonatanmartir@gmail.com>
2018-11-14 22:46:04 -0600

    se agregaron los Fonts

--- src/splash/SplashController.java
+++ src/splash/SplashController.java
@@ -10,6 +10,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
